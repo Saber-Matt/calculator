5e9fe07788fb595aba83e2bea4d872667b517a3d
Matthew Saber <mattsaber21@yahoo.com>
2021-02-11 10:50:12 -0800

    finished feature

--- app.js
+++ app.js
@@ -3,3 +3,14 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const addOne = document.getElementById('first-number');
+const addTwo = document.getElementById('second-number');
+const addButton = document.getElementById('add-button');
+const solution = document.getElementById('solution');
+
+
+addButton.addEventListener('click', () => {
+    const sum = Number(addOne.value) + Number(addTwo.value);
+    solution.textContent = sum;
+});

78ac6973aa9f1f6df2021b87d1634e9fa1350d41
Matthew Saber <mattsaber21@yahoo.com>
2021-02-11 16:22:58 -0800

    refactoring progress

--- app.js
+++ app.js
@@ -4,13 +4,49 @@
 
 // set event listeners to update state and DOM
 
-const addOne = document.getElementById('first-number');
-const addTwo = document.getElementById('second-number');
-const addButton = document.getElementById('add-button');
-const solution = document.getElementById('solution');
+import { add } from '../utilities.js';  
 
+
+const addOne = document.getElementById('add-one');
+const addTwo = document.getElementById('add-two');
+const addButton = document.getElementById('add-button');
+const addition = document.getElementById('addition');
 
 addButton.addEventListener('click', () => {
-    const sum = Number(addOne.value) + Number(addTwo.value);
-    solution.textContent = sum;
+    const x = Number(addOne.value);
+    const y = Number(addTwo.value);
+    const sum = add(x, y);
+    addition.textContent = sum;
+ 
+
+});
+
+const subOne = document.getElementById('sub-one');
+const subTwo = document.getElementById('sub-two');
+const subButton = document.getElementById('sub-button');
+const subtraction = document.getElementById('subtraction');
+
+subButton.addEventListener('click', () => {
+    const sub = Number(subOne.value) - Number(subTwo.value);
+    subtraction.textContent = sub;
+});
+
+const multiOne = document.getElementById('multi-one');
+const multiTwo = document.getElementById('multi-two');
+const multiButton = document.getElementById('multi-button');
+const multiplication = document.getElementById('multiplication');
+
+multiButton.addEventListener('click', () => {
+    const multiply = Number(multiOne.value) * Number(multiTwo.value);
+    multiplication.textContent = multiply;
+});
+
+const divOne = document.getElementById('div-one');
+const divTwo = document.getElementById('div-two');
+const divButton = document.getElementById('div-button');
+const division = document.getElementById('division');
+
+divButton.addEventListener('click', () => {
+    const divide = Number(divOne.value) / Number(divTwo.value);
+    division.textContent = divide;
 });
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,28 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add } from '../utilities.js';
+import { sub } from '../utilities.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 4;
+    const y = 5;
+    const sum = 9;
+
+    const a = 4;
+    const b = 5;
+    const minus = -1;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const result = add(x, y);
+    const leftOver = sub(a, b);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(result, sum);
+    expect.equal(leftOver, minus);
 });
